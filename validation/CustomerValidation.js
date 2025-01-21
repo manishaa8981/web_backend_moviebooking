5388f7c19f4a90d8c6778632d5cf27e5a866dd90
manishaa8981 <manishaa8981@gmail.com>
2025-01-21 14:20:09 +0545

    added images

--- validation/CustomerValidation.js
+++ validation/CustomerValidation.js
@@ -4,11 +4,17 @@ const customerSchema = joi.object({
   full_name: joi.string().required(),
   email: joi.string().required().email(),
   contact_no: joi.string().required(),
+  image: joi.string(),
 });
 
 function CustomerValidation(req, res, next) {
-  const { full_name, email, contact_no } = req.body;
-  const { error } = customerSchema.validate({ full_name, email, contact_no });
+  const { full_name, email, contact_no, image } = req.body;
+  const { error } = customerSchema.validate({
+    full_name,
+    email,
+    contact_no,
+    image,
+  });
   if (error) {
     return res.json(error);
   }
