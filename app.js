cb5ee0bdd4aa93491316057732edf1abe49a67a3
manishaa8981 <manishaa8981@gmail.com>
2025-01-21 14:17:03 +0545

    ..

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const SeatRoute = require("./routes/SeatRoute");
 const ShowTimeRoute = require("./routes/ShowTimeRoute");
 const HallRoute = require("./routes/HallRoute");
 const BookingRoute = require("./routes/BookingRoute");
+const AuthRoute = require("./routes/AuthRoute");
+
+const cors = require("cors");
 
 connectDB();
 const app = express();
 
 app.use(express.json());
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.use("/api/customer", CustomerRoute);
 app.use("/api/movie", MovieRoute);
@@ -18,6 +24,7 @@ app.use("/api/seat", SeatRoute);
 app.use("/api/showTime", ShowTimeRoute);
 app.use("/api/hall", HallRoute);
 app.use("/api/booking", BookingRoute);
+app.use("/api/auth", AuthRoute);
 
 const port = 4011;
 app.listen(port, () => {
